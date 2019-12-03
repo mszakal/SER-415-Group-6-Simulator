5efdb1bc57f5eb81ad3db54569e173ff26a156f3
gotzerbie <34197482+gotzerbie@users.noreply.github.com>
2019-12-02 20:44:36 -0700

    new semesterproject

--- semesterProject.py
+++ semesterProject.py
@@ -1,12 +1,13 @@
 import tkinter
-<<<<<<< HEAD
+from tkinter import *
+
 def change_pic(labelname, picture):
     photo1 = ImageTk.PhotoImage(Image.open("model1.png"))
     labelname.configure(image=photo1)
     labelname.photo = photo1
 
-    
-    
+
+
 def model1():
     novi = Toplevel()
     canvas = Canvas(novi, width = 300, height = 200)
@@ -16,7 +17,7 @@ def model1():
     canvas.create_image(50, 10, image = gif1, anchor = NW)
     #assigned the gif1 to the canvas object
     canvas.gif1 = gif1
-    
+
 def model2():
     novi = Toplevel()
     canvas = Canvas(novi, width = 300, height = 200)
@@ -26,7 +27,7 @@ def model2():
     canvas.create_image(50, 10, image = gif1, anchor = NW)
     #assigned the gif1 to the canvas object
     canvas.gif1 = gif1
-    
+
 def model3():
     novi = Toplevel()
     canvas = Canvas(novi, width = 300, height = 200)
@@ -36,7 +37,7 @@ def model3():
     canvas.create_image(50, 10, image = gif1, anchor = NW)
     #assigned the gif1 to the canvas object
     canvas.gif1 = gif1
-    
+
 def model4():
     novi = Toplevel()
     canvas = Canvas(novi, width = 300, height = 200)
@@ -46,11 +47,6 @@ def model4():
     canvas.create_image(50, 10, image = gif1, anchor = NW)
     #assigned the gif1 to the canvas object
     canvas.gif1 = gif1
-    
-=======
-from tkinter import *
-
->>>>>>> 6019120253bb6f6cd75f6690ec1e858db50dde1b
 
 def calculate():
     forward_multiplier = .7
@@ -87,29 +83,21 @@ def calculate():
     east_out_display.configure(text=str(east_out))
     west_out_display.configure(text=str(west_out))
 
-
-<<<<<<< HEAD
-from tkinter import *
 import tkinter as tk
 from tkinter import ttk
-=======
->>>>>>> 6019120253bb6f6cd75f6690ec1e858db50dde1b
 root = tkinter.Tk()
+model = IntVar()
 root.geometry('1000x800')
 DEFAULT = "model_images/model1.png"
 MODEL2 = "model_images/model2.png"
 MODEL3 = "model_images/model3.png"
 MODEL4 = "model_images/model4.png"
 
-model = IntVar()
-#photo_filepath = tk.StringVar()
-
-<<<<<<< HEAD
 model1 = tkinter.Radiobutton(root, text='Model 1', variable=model, value=1, command=model1).grid(row=0)
 model2 = tkinter.Radiobutton(root, text='Model 2', variable=model, value=2, command=model2).grid(row=1)
 model3 = tkinter.Radiobutton(root, text='Model 3', variable=model, value=3, command=model3).grid(row=2)
 model4 = tkinter.Radiobutton(root, text='Model 4', variable=model, value=4, command=model4).grid(row=3)
-=======
+
 north_in = tkinter.Entry(root)
 south_in = tkinter.Entry(root)
 east_in = tkinter.Entry(root)
@@ -119,13 +107,6 @@ north_out_display = tkinter.Label(root)
 south_out_display = tkinter.Label(root)
 east_out_display = tkinter.Label(root)
 west_out_display = tkinter.Label(root)
-
-model = IntVar()
-tkinter.Radiobutton(root, text='Model 1', variable=model, value=1).grid(row=0)
-tkinter.Radiobutton(root, text='Model 2', variable=model, value=2).grid(row=1)
-tkinter.Radiobutton(root, text='Model 3', variable=model, value=3).grid(row=2)
-tkinter.Radiobutton(root, text='Model 4', variable=model, value=4).grid(row=3)
->>>>>>> 6019120253bb6f6cd75f6690ec1e858db50dde1b
 
 tkinter.Label(root, text='Inflow from North').grid(row=0, column=1, sticky=E)
 tkinter.Label(root, text='Inflow from South').grid(row=1, column=1, sticky=E)
@@ -146,11 +127,6 @@ south_out_display.grid(row=6, column=1)
 tkinter.Label(root, text='Eastbound Output: ').grid(row=7, column=0, sticky=E)
 east_out_display.grid(row=7, column=1)
 tkinter.Label(root, text='Westbound Output: ').grid(row=8, column=0, sticky=E)
-<<<<<<< HEAD
-westOutDisp = tkinter.Label(root)
-westOutDisp.grid(row=8, column=1)
-=======
 west_out_display.grid(row=8, column=1)
 
->>>>>>> 6019120253bb6f6cd75f6690ec1e858db50dde1b
 root.mainloop()
